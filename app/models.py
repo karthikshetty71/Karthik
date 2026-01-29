f1e71c86505967ac6581edd312622e9df3fd40ca
kshetty18 <karthikrj71@gmail.com>
2026-01-29 20:22:26 +0530

    add enable/disable user table to db

--- app/models.py
+++ app/models.py
@@ -10,6 +10,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+    # Active Status (Default is True/Active)
+        is_active = db.Column(db.Boolean, default=True)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
